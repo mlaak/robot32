214a5dca71359085c6a8a78c57f58453592ded87
Martin Laak <martin.laak@gmail.com>
2024-06-27 17:46:10 +0200

    lib installer

---
+++ html/experts/general/install_libs.bash
@@ -0,0 +1,10 @@
+# composer does not support git subdirectories on monorepos 
+# (despite 9 years people asking for it)
+# But don't worry, there is a workaround - if you use their paid product :D
+# 
+# So yeah, we do just use bash scripts for installing some stuff here
+
+bash <(curl -s "https://raw.githubusercontent.com/mlaak/robot32lib/main/vendor/Robot32lib/Middleware/install.bash")
+
+
+
