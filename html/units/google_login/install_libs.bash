f70eacd677f89bdd9d56229aec9f0f60cf4c7299
mlaak <martin.laak@gmail.com>
2024-07-21 00:23:18 +0200

    composer files

---
+++ html/units/google_login/install_libs.bash
@@ -0,0 +1,7 @@
+# composer does not support git subdirectories on monorepos 
+# (despite 9 years people asking for it)
+# But don't worry, there is a workaround - if you use their paid product :D
+# 
+# So yeah, we do just use bash scripts for installing some stuff here.
+# Other stuff installs well with composer so use that in that case.
+
